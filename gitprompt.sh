c6386c7b53ce304e2fe215eef930dcad52d343be
icedwater <icedwater@gmail.com>
2017-12-14 22:01:51 +0800

    Used cut in place of awk to print whole branch name.

--- gitprompt.sh
+++ gitprompt.sh
@@ -1,7 +1,7 @@
 # check if it is a git repository, then show current branch, thanks to
 # //www.leaseweb.com/labs/2013/08/git-tip-show-your-branch-name-on-the-linux-prompt/
 parse_git_branch () { 
-    BRANCH=$(git branch 2> /dev/null | grep \* | awk '{print $2}');
+    BRANCH=$(git branch 2> /dev/null | grep \* | cut -d' ' -f2-);
     CHANGES=$(git status --porcelain --untracked-files=no 2> /dev/null | wc -l);
     CLEAN=${BRANCH}
     DIRTY=${BRANCH}:${CHANGES}
