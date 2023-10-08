6875838756202c2af0981be8a7ea8107e34b686f
icedwater <icedwater@gmail.com>
2023-10-08 09:52:44 +0800

    Specified coordinates instead of using --right-of.
    - --q12 was not needed even though pos is a RandR 1.2 argument
    - --right-of, --left-of seem to override any pos specified

--- i3/scripts/monitor.sh
+++ i3/scripts/monitor.sh
@@ -1,6 +1,6 @@
 if [[ $(xrandr | grep -c "HDMI1 connected") -eq 1 ]]; then
-    xrandr --output eDP1 --primary \
-           --output HDMI1 --auto --rotate right --right-of eDP1;
+    xrandr --output eDP1 --primary --pos 0x1152 \
+           --output HDMI1 --auto --rotate right --pos 1366x0;
 else
     xrandr --output eDP1 --primary;
     xrandr --output HDMI1 --off;
