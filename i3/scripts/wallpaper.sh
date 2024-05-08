9c8c89cff21423b4aebb85cec57f5e5f8ed497ab
icedwater <icedwater@gmail.com>
2024-05-08 15:19:44 +0800

    Updated wallpaper change script.

--- i3/scripts/wallpaper.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# assumes feh is installed
-# second picture is for second monitor, comment as needed
-
-PICS_FOLDER='/path/to/Pictures'
-
-feh --no-fehbg --randomize --bg-fill ${PICS_FOLDER} \
-     -no-fehbg --randomize --bg-fill ${PICS_FOLDER}
